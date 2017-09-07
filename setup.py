0f6c75d8b5d435c46daf8c49e2a924a6bc9424e6
Cédric Le Varlet <cedriclv@gmail.com>
2017-09-07 12:06:46 +0200

    Updated version number

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='ConfLoad',
-    version='1.0',
+    version='1.0.1',
     description='Configuration file loader and parser',
     long_description=open('README.md').read(),
     author='Newcraft',
