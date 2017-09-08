32d04098223231a32be400ed9662619788b5c1ad
Cédric Le Varlet <cedriclv@gmail.com>
2017-09-08 11:49:10 +0200

    Prefixed file extension check with a .

--- confload/__init__.py
+++ confload/__init__.py
@@ -13,11 +13,11 @@ class Config:
 
     @staticmethod
     def load(file: str):
-        if "ini" in file:
+        if ".ini" in file:
             Config._instance = IniConfig
-        if "json" in file:
+        if ".json" in file:
             Config._instance = JsonConfig
-        if "yaml" in file or "yml" in file:
+        if ".yaml" in file or ".yml" in file:
             Config._instance = YamlConfig
 
         Config._instance.load(file)
